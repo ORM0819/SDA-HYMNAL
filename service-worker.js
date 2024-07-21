7ff245836e53f4058240228c2df11af2c22a45d9
ORM0819 <124946139+ORM0819@users.noreply.github.com>
2024-07-21 17:37:21 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sda-hymnal-cache-v1';
+const URLS_TO_CACHE = [
+    '/',
+    'index.html',
+    'image.html',
+    'styles.css',
+    'manifest.json',
+    // Add other assets you want to cache
+    'src/Hymnal.XF/Resources/Assets/MusicSheets/PianoSheet_NewHymnal_en_001.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(URLS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
