5eae10cb7b08aff53278bcf060e9692803f032ce
ORMT03 <124946139+ORMT03@users.noreply.github.com>
2024-07-25 20:38:12 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -134,5 +134,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // Load initial data
     Promise.all([loadSongs(), loadSongMapping()]);
 
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/SDA-HYMNAL/service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
     
 });
