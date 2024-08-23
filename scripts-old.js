b2491a266b0b378b9fe098553fe749282bfcdc6e
ORMT03 <124946139+ORMT03@users.noreply.github.com>
2024-08-22 19:58:26 -0500

    Create scripts-old.js

---
+++ scripts-old.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById('search');
+    var songList = document.getElementById('song-list');
+    var dropdownMenu = document.getElementById('dropdown-menu');
+    var languageDropdown = document.getElementById('language-dropdown');
+    var startCycleButton = document.getElementById('start-cycle');
+    
+    var allSongs = [];
+    var songMapping = [];
+
+    function loadSongMapping() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'song_mapping.json', true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                songMapping = JSON.parse(xhr.responseText);
+            }
+        };
+        xhr.send();
+    }
+
+    function getSongsUrls() {
+        var language = languageDropdown.value;
+        switch (language) {
+            case 'spanish':
+                return [{ url: 'songs_es.json', language: 'spanish' }];
+            case 'both':
+                return [
+                    { url: 'songs.json', language: 'english' },
+                    { url: 'songs_es.json', language: 'spanish' }
+                ];
+            default:
+                return [{ url: 'songs.json', language: 'english' }];
+        }
+    }
+
+    function loadSongs() {
+        var urls = getSongsUrls();
+        var completedRequests = 0;
+        var tempSongs = [];
+
+        urls.forEach(function (item) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', item.url, true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText);
+                    data.forEach(function (song) {
+                        song.language = item.language;
+                        tempSongs.push(song);
+                    });
+                    completedRequests++;
+                    if (completedRequests === urls.length) {
+                        allSongs = tempSongs;
+                        populateList(allSongs);
+                    }
+                }
+            };
+            xhr.send();
+        });
+    }
+
+    function populateList(songs) {
+        songList.innerHTML = '';
+        songs.forEach(function (song) {
+            var li = document.createElement('li');
+            li.textContent = song.number + ' - ' + song.title;
+            li.dataset.image = song.image;
+            li.dataset.title = song.title;
+            li.dataset.number = song.number;
+            li.dataset.content = song.content;
+            li.addEventListener('click', function () {
+                var imageUrl = 'src/Hymnal.XF/Resources/Assets/MusicSheets/' + song.image;
+                var title = encodeURIComponent(song.title);
+                var number = encodeURIComponent(song.number);
+                var content = encodeURIComponent(song.content);
+
+                var page = dropdownMenu.value === 'lyrics' ? 'lyrics.html' : 'image.html';
+                window.location.href = page + '?' + (dropdownMenu.value === 'lyrics' ? 'content=' + content + '&' : 'image=' + encodeURIComponent(imageUrl) + '&') + 'title=' + title + '&number=' + number;
+            });
+            songList.appendChild(li);
+        });
+    }
+
+    searchInput.addEventListener('input', function () {
+        var query = searchInput.value.toLowerCase();
+        var filteredSongs = allSongs.filter(function (song) {
+            return song.number.toLowerCase().indexOf(query) !== -1 || song.title.toLowerCase().indexOf(query) !== -1;
+        });
+
+        var mappedSongs = [];
+        filteredSongs.forEach(function (song) {
+            mappedSongs.push(song);
+            var mapping = songMapping.find(function (map) {
+                return map.english === song.number || map.spanish === song.number;
+            });
+            if (mapping) {
+                var correspondingNumber = mapping.english === song.number ? mapping.spanish : mapping.english;
+                var correspondingSong = allSongs.find(function (s) {
+                    return s.number === correspondingNumber && s.language === (mapping.english === song.number ? 'spanish' : 'english');
+                });
+                if (correspondingSong) {
+                    mappedSongs.push(correspondingSong);
+                }
+            }
+        });
+
+        populateList(mappedSongs);
+    });
+
+    startCycleButton.addEventListener('click', function () {
+        localStorage.setItem('currentIndex', 0);
+        var page = dropdownMenu.value === 'lyrics' ? 'start-cycle-lyrics.html' : 'start-cycle.html';
+        window.location.href = page;
+    });
+
+    loadSongs();
+    loadSongMapping();
+});
