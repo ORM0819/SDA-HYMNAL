81627c6500415a327e748393b72ec6a5e89cfee0
ORMT03 <124946139+ORMT03@users.noreply.github.com>
2024-08-24 13:42:03 -0500

    Update scripts-old.js

--- scripts-old.js
+++ scripts-old.js
@@ -20,16 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function getSongsUrls() {
         var language = languageDropdown.value;
-        switch (language) {
-            case 'spanish':
-                return [{ url: 'songs_es.json', language: 'spanish' }];
-            case 'both':
-                return [
-                    { url: 'songs.json', language: 'english' },
-                    { url: 'songs_es.json', language: 'spanish' }
-                ];
-            default:
-                return [{ url: 'songs.json', language: 'english' }];
+        if (language === 'spanish') {
+            return [{ url: 'songs_es.json', language: 'spanish' }];
+        } else if (language === 'both') {
+            return [
+                { url: 'songs.json', language: 'english' },
+                { url: 'songs_es.json', language: 'spanish' }
+            ];
+        } else {
+            return [{ url: 'songs.json', language: 'english' }];
         }
     }
 
@@ -38,30 +37,33 @@ document.addEventListener('DOMContentLoaded', function () {
         var completedRequests = 0;
         var tempSongs = [];
 
-        urls.forEach(function (item) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', item.url, true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    var data = JSON.parse(xhr.responseText);
-                    data.forEach(function (song) {
-                        song.language = item.language;
-                        tempSongs.push(song);
-                    });
-                    completedRequests++;
-                    if (completedRequests === urls.length) {
-                        allSongs = tempSongs;
-                        populateList(allSongs);
+        for (var i = 0; i < urls.length; i++) {
+            (function(item) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', item.url, true);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText);
+                        for (var j = 0; j < data.length; j++) {
+                            data[j].language = item.language;
+                            tempSongs.push(data[j]);
+                        }
+                        completedRequests++;
+                        if (completedRequests === urls.length) {
+                            allSongs = tempSongs;
+                            populateList(allSongs);
+                        }
                     }
-                }
-            };
-            xhr.send();
-        });
+                };
+                xhr.send();
+            })(urls[i]);
+        }
     }
 
     function populateList(songs) {
         songList.innerHTML = '';
-        songs.forEach(function (song) {
+        for (var i = 0; i < songs.length; i++) {
+            var song = songs[i];
             var li = document.createElement('li');
             li.textContent = song.number + ' - ' + song.title;
             li.dataset.image = song.image;
@@ -69,54 +71,58 @@ document.addEventListener('DOMContentLoaded', function () {
             li.dataset.number = song.number;
             li.dataset.content = song.content;
             li.addEventListener('click', function () {
-                var imageUrl = 'src/Hymnal.XF/Resources/Assets/MusicSheets/' + song.image;
-                var title = encodeURIComponent(song.title);
-                var number = encodeURIComponent(song.number);
-                var content = encodeURIComponent(song.content);
+                var imageUrl = 'src/Hymnal.XF/Resources/Assets/MusicSheets/' + this.dataset.image;
+                var title = encodeURIComponent(this.dataset.title);
+                var number = encodeURIComponent(this.dataset.number);
+                var content = encodeURIComponent(this.dataset.content);
 
                 var page = dropdownMenu.value === 'lyrics' ? 'lyrics-old.html' : 'image-old.html';
                 window.location.href = page + '?' + (dropdownMenu.value === 'lyrics' ? 'content=' + content + '&' : 'image=' + encodeURIComponent(imageUrl) + '&') + 'title=' + title + '&number=' + number;
             });
             songList.appendChild(li);
-        });
+        }
     }
 
     function normalizeText(text) {
-        // Replace accented characters with their non-accented equivalents
-        return text.replace(/[áàäâã]/g, 'a')
-                   .replace(/[éèëê]/g, 'e')
-                   .replace(/[íìïî]/g, 'i')
-                   .replace(/[óòöôõ]/g, 'o')
-                   .replace(/[úùüû]/g, 'u')
-                   .replace(/[ñ]/g, 'n')
-                   .replace(/[^\w\s]/gi, ''); // Remove punctuation
+        return text.replace(/[àáâãäå]/g,"a")
+                   .replace(/[èéêë]/g,"e")
+                   .replace(/[ìíîï]/g,"i")
+                   .replace(/[òóôõö]/g,"o")
+                   .replace(/[ùúûü]/g,"u")
+                   .replace(/ñ/g,"n")
+                   .replace(/[^\w\s]/g, ''); // Remove punctuation
     }
 
     searchInput.addEventListener('input', function () {
         var query = normalizeText(searchInput.value.toLowerCase());
-        var filteredSongs = allSongs.filter(function (song) {
-            return normalizeText(song.number.toLowerCase()).indexOf(query) !== -1 || 
-                   normalizeText(song.title.toLowerCase()).indexOf(query) !== -1;
-        });
+        var filteredSongs = [];
+        
+        for (var i = 0; i < allSongs.length; i++) {
+            var song = allSongs[i];
+            if (normalizeText(song.number.toLowerCase()).indexOf(query) !== -1 || 
+                normalizeText(song.title.toLowerCase()).indexOf(query) !== -1) {
+                filteredSongs.push(song);
 
-        var mappedSongs = [];
-        filteredSongs.forEach(function (song) {
-            mappedSongs.push(song);
-            var mapping = songMapping.find(function (map) {
-                return map.english === song.number || map.spanish === song.number;
-            });
-            if (mapping) {
-                var correspondingNumber = mapping.english === song.number ? mapping.spanish : mapping.english;
-                var correspondingSong = allSongs.find(function (s) {
-                    return s.number === correspondingNumber && s.language === (mapping.english === song.number ? 'spanish' : 'english');
-                });
-                if (correspondingSong) {
-                    mappedSongs.push(correspondingSong);
+                // Check for corresponding song mapping
+                for (var j = 0; j < songMapping.length; j++) {
+                    var mapping = songMapping[j];
+                    if (mapping.english === song.number || mapping.spanish === song.number) {
+                        var correspondingNumber = mapping.english === song.number ? mapping.spanish : mapping.english;
+                        for (var k = 0; k < allSongs.length; k++) {
+                            var correspondingSong = allSongs[k];
+                            if (correspondingSong.number === correspondingNumber && 
+                                correspondingSong.language === (mapping.english === song.number ? 'spanish' : 'english')) {
+                                filteredSongs.push(correspondingSong);
+                                break;
+                            }
+                        }
+                        break;
+                    }
                 }
             }
-        });
+        }
 
-        populateList(mappedSongs);
+        populateList(filteredSongs);
     });
 
     languageDropdown.addEventListener('change', function () {
